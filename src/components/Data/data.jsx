1e050e4ae70664b48ba083f552ddd6c5d9d5bc75
EROSION6 <147639244+EROSION6@users.noreply.github.com>
2024-01-27 14:00:21 +0300

    landing-1
    landing-1

---
+++ src/components/Data/data.jsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import { IoCardOutline } from 'react-icons/io5';
+import { FaShieldHalved } from 'react-icons/fa6';
+import { RxLightningBolt } from 'react-icons/rx';
+
+export const cardBlockInfo = [
+	{
+		icon: <IoCardOutline />,
+		imgUrl:
+			'https://framerusercontent.com/images/5MJ2KFCUhx9m3zYnxRjmhiQUt8.webp?scale-down-to=512',
+		title: 'Transaction Flexibility',
+		info: 'Accept various payment methods and allow customers to play conveniently',
+	},
+	{
+		icon: <RxLightningBolt />,
+		imgUrl:
+			'https://framerusercontent.com/images/FRaA1sWclx1ypRWRjy4vEd0DXE.webp?scale-down-to=512',
+		title: 'Optimized Handling',
+		info: 'Faster approvals and smoother transactions to elevate user experience.',
+	},
+	{
+		icon: <FaShieldHalved />,
+		imgUrl:
+			'https://framerusercontent.com/images/heEC6zDmKnKXHqeVgrwSS1Bg2m8.webp?scale-down-to=512',
+		title: 'Fraud Protection',
+		info: 'Advanced fraud prevention tools to safeguard business and customers.',
+	},
+];
+
+export const advertising = [
+	{
+		icon: '/public/icon/trello-svgrepo-com (1).svg',
+		text: 'Trello',
+	},
+	{
+		icon: '/public/icon/slack-svgrepo-com.svg',
+		text: 'Slack',
+	},
+	{
+		icon: '/public/icon/maze-svgrepo-com.svg',
+		text: 'Maze',
+	},
+	{
+		icon: '/public/icon/dropbox-158-svgrepo-com.svg',
+		text: 'Dropbox',
+	},
+	{
+		icon: '/public/icon/webflow-svgrepo-com.svg',
+		text: 'WebFlow',
+	},
+	{
+		icon: '/public/icon/doordash-svgrepo-com.svg',
+		text: 'DoorDash',
+	},
+	{
+		icon: '/public/icon/contentful-svgrepo-com.svg',
+		text: 'Contentful',
+	},
+	{
+		icon: '/public/icon/trello-svgrepo-com (1).svg',
+		text: 'Trello',
+	},
+	{
+		icon: '/public/icon/slack-svgrepo-com.svg',
+		text: 'Slack',
+	},
+	{
+		icon: '/public/icon/maze-svgrepo-com.svg',
+		text: 'Maze',
+	},
+	{
+		icon: '/public/icon/dropbox-158-svgrepo-com.svg',
+		text: 'Dropbox',
+	},
+	{
+		icon: '/public/icon/webflow-svgrepo-com.svg',
+		text: 'WebFlow',
+	},
+	{
+		icon: '/public/icon/doordash-svgrepo-com.svg',
+		text: 'DoorDash',
+	},
+	{
+		icon: '/public/icon/contentful-svgrepo-com.svg',
+		text: 'Contentful',
+	},
+];
+
+export const firmaContentList = [
+	{
+		icon: '/public/Reclamation/1.webp',
+		name: 'Mastercard',
+	},
+	{
+		icon: '/public/Reclamation/2.webp',
+		name: 'Visa',
+	},
+	{
+		icon: '/public/Reclamation/3.webp',
+		name: 'American Express',
+	},
+	{
+		icon: '/public/Reclamation/4.webp',
+		name: 'Paypal',
+	},
+	{
+		icon: '/public/Reclamation/5.webp',
+		name: 'Google Pay',
+	},
+	{
+		icon: '/public/Reclamation/6.webp',
+		name: 'Apple Pay',
+	},
+	{
+		icon: '/public/Reclamation/7.webp',
+		name: 'Union Pay',
+	},
+	{
+		icon: '/public/Reclamation/8.webp',
+		name: 'Meta Pay',
+	},
+	{
+		icon: '/public/Reclamation/9.webp',
+		name: 'Alipay',
+	},
+	{
+		icon: '/public/Reclamation/10.webp',
+		name: 'Skrill',
+	},
+	{
+		icon: '/public/Reclamation/11.webp',
+		name: 'Affirm',
+	},
+	{
+		icon: '/public/Reclamation/12.webp',
+		name: 'Amazon Pay',
+	},
+];
+
+export const AccordionInfo = [
+	{
+		title: 'What payment methods do you support?',
+		text: 'We accept various payment methods, including credit cards, bank transfers, and digital wallets.',
+	},
+	{
+		title: 'How secure are transactions on Oritsu',
+		text: 'Our platform ensures robust security protocols, utilizing encryption and advanced security measures to safeguard your transactions.',
+	},
+	{
+		title: 'Can I access Oritsu from my mobile device?',
+		text: 'Yes, Oritsu is designed to be accessible and user-friendly on all devices, including smartphones and tablets.',
+	},
+	{
+		title: 'What type of customer support do you offer?',
+		text: 'We offer dedicated customer support available round the clock to assist you with any queries or issues you may encounter.',
+	},
+	{
+		title: 'Is Oritsu compliant with financial regulations?',
+		text: 'Yes, we strictly adhere to financial regulations and compliance standards, ensuring a secure and compliant platform for our users.',
+	},
+];
